6e1c0005225045623a5e7a62619afae9ad2b482c
june.kim <june.kim@kakaocorp.com>
2018-06-14 23:08:14 +0900

    6장 돌아온 '모두를 위한 평등'

--- TODO.md
+++ TODO.md
@@ -9,5 +9,6 @@
 * [ ] Equal object
 * [x] 5CHF * 2 = 10CHF
 * [ ] Dollar/Franc 중복
-* [ ] 공용 equals
+* [x] 공용 equals
 * [ ] 공용 times
+* [ ] Franc와 Dollar 비교하기
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -21,5 +21,7 @@ public class MoneyTest {
     public void testEquality() {
         assertThat(new Dollar(5)).isEqualTo(new Dollar(5));
         assertThat(new Dollar(5)).isNotEqualTo(new Dollar(6));
+        assertThat(new Franc(5)).isEqualTo(new Franc(5));
+        assertThat(new Franc(5)).isNotEqualTo(new Franc(6));
     }
 }
